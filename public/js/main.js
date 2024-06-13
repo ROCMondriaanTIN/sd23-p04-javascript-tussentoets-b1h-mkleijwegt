c8944f731046724c47bf259e70581e87379fb099
Mark Kleijwegt <mark.kleijwegt@gmail.com>
2024-06-13 14:50:11 +0200

    Added chart as example for Dhakir

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
 console.log('Main loaded');
 
 const cardRow = document.querySelector('.card-container');
+const priceChart = document.querySelector('.price-chart');
+const priceChartPie = document.querySelector('.price-chart-pie');
 
 fetch('/icecreams/')
     .then(data => data.json())
@@ -9,12 +11,20 @@ fetch('/icecreams/')
 function showIceCreams(icecreams){
     console.log(icecreams);
 
+    const labels = [];
+    const data = [];
     let htmlCode = '';
     for (let i = 0; i < icecreams.length; i++) {
         const icecream = icecreams[i];
         htmlCode += createCard(icecream);
+        labels.push(icecream.icecream);
+        data.push(icecream.price);
     }
     cardRow.innerHTML = htmlCode;
+    console.log(labels);
+    console.log(data);
+    createChart(priceChart, labels, data, 'Prijs per ijsje', 'bar');
+    createChart(priceChartPie, labels, data, 'Prijs per ijsje', 'pie');
 }
 
 function createCard(icecream){
@@ -29,4 +39,27 @@ function createCard(icecream){
             </div>
         </div>`;
     return card;
+}
+
+function createChart(canvas, labels, data, label, type){
+    new Chart(canvas, {
+        type: type,
+        data: {
+          //de labels komen op de x as
+          labels: labels,
+          datasets: [{
+            label: label,
+            // de data komt op de y as
+            data: data,
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        }
+    });
 }
